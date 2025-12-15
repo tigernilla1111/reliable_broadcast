7a95e328f1621b271097d45dba669ec346f19757
Gilad <gilad@sundaygroupinc.com>
2025-12-15 12:03:52 -0800

    add Interface and connect logic http

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,17 +1,19 @@
 use jsonrpsee::core::{RpcResult, async_trait};
 use jsonrpsee::proc_macros::rpc;
 use jsonrpsee::{MethodCallback, MethodResponse, Methods, RpcModule, server};
+use std::collections::HashMap;
 use std::{net::SocketAddr, sync::Arc, time::Duration};
 use tokio;
 
 use jsonrpsee::core::client::ClientT;
-use jsonrpsee::http_client::HttpClientBuilder;
+use jsonrpsee::http_client::{HttpClient, HttpClientBuilder};
 use jsonrpsee::rpc_params;
 use jsonrpsee::server::ServerBuilder;
 use tokio::sync::{Mutex, mpsc, watch};
 use tokio::time::sleep;
 // use tower::limit::ConcurrencyLimitLayer;
 use types::UserId;
+
 #[derive(Debug)]
 enum Data {
     Init,
@@ -19,7 +21,9 @@ enum Data {
     DirectMessage(UserId, String),
 }
 
-pub struct DataCache {
+// TODO: organize the data into purpose.  ie dm messages will go into DataCache::dms and ledger data will go into DataCache::ledger
+// I can get rid of enum Data at this point and just store a vector of structs with info for each data type
+struct DataCache {
     data: Vec<Data>,
 }
 
@@ -31,7 +35,7 @@ mod api {
         #[method(name = "ping")]
         async fn ping(&self) -> RpcResult<String>;
         #[method(name = "dm")]
-        async fn dm(&self, msg: &str) -> RpcResult<()>;
+        async fn dm(&self, msg: DirectMessage) -> RpcResult<()>;
     }
 
     pub struct RpcServerImpl {
@@ -53,20 +57,37 @@ mod api {
         async fn ping(&self) -> RpcResult<String> {
             Ok(format!("pong"))
         }
-        async fn dm(&self, msg: &str) -> RpcResult<()> {
-            let data = Data::DirectMessage(0u64, msg.to_string());
+        async fn dm(&self, dm: DirectMessage) -> RpcResult<()> {
+            let data = Data::DirectMessage(dm.sender, dm.msg.to_string());
             self.add_data(data).await;
             Ok(())
+        }
+    }
+
+    #[derive(serde::Serialize, serde::Deserialize)]
+    pub struct DirectMessage {
+        sender: UserId,
+        msg: String,
+    }
+    impl DirectMessage {
+        pub fn new(sender: UserId, msg: &str) -> Self {
+            DirectMessage {
+                sender,
+                msg: msg.to_string(),
+            }
         }
     }
 }
 
 use api::{MyRpcClient, MyRpcServer, RpcServerImpl};
-pub async fn server() -> (SocketAddr, watch::Receiver<()>, Arc<Mutex<DataCache>>) {
+
+use crate::api::DirectMessage;
+async fn server() -> (SocketAddr, watch::Receiver<()>, Arc<Mutex<DataCache>>) {
     let (sndr, rcvr) = watch::channel(());
     let cache = Arc::new(Mutex::new(DataCache { data: Vec::new() }));
     let server_impl = RpcServerImpl::new(sndr, cache.clone());
     let server = ServerBuilder::default()
+        // TODO vvv
         // .set_http_middleware(tower::ServiceBuilder::new()
         //.layer(ConcurrencyLimitLayer::new(1000))
         //.layer(RequestBodyLimitLayer::new(1024 * 1024)) // 1 MB
@@ -83,11 +104,69 @@ pub async fn server() -> (SocketAddr, watch::Receiver<()>, Arc<Mutex<DataCache>>
     (addr, rcvr, cache)
 }
 
+pub struct Interface {
+    /// My IpAddr
+    _addr: SocketAddr,
+    /// TODO: My PubKey
+    pubkey: UserId,
+    /// Watch channel thats updates when new items are received over the network
+    new_data_notifee: watch::Receiver<()>,
+    /// Data from the network
+    cache: Arc<Mutex<DataCache>>,
+    /// Maps a UserId to their addr
+    addr_book: HashMap<UserId, SocketAddr>,
+    /// Stores clients so I don't have to recreate them for each outgoing connection
+    clients: HashMap<SocketAddr, Arc<HttpClient>>,
+}
+
+impl Interface {
+    pub async fn start() -> Self {
+        let (addr, new_data_notifee, cache) = server().await;
+        Self {
+            _addr: addr,
+            new_data_notifee: new_data_notifee,
+            cache,
+            addr_book: HashMap::new(),
+            pubkey: UserId::new(),
+            clients: HashMap::new(),
+        }
+    }
+    pub async fn dm(&mut self, rcvr: &UserId, msg: &str) {
+        let client = self.connect(rcvr).await;
+        let dm = DirectMessage::new(self.pubkey, msg);
+        client.dm(dm).await.unwrap();
+    }
+    pub async fn connect(&mut self, rcvr: &UserId) -> Arc<HttpClient> {
+        let addr = self.addr_book.get(rcvr).unwrap();
+        let client = self.clients.entry(*addr).or_insert_with(|| {
+            let server_url = format!("http://{}", addr);
+            Arc::new(HttpClientBuilder::default().build(&server_url).unwrap())
+        });
+        client.clone()
+    }
+    pub async fn add_addr_book(&mut self, id: UserId, addr: SocketAddr) {
+        self.addr_book.insert(id, addr);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use jsonrpsee::RpcModule;
 
+    use crate::api::DirectMessage;
+
     use super::*;
+
+    #[tokio::test]
+    async fn interface_test() {
+        let iface = Interface::start().await;
+        let server_addr = iface._addr;
+        let notifee = iface.new_data_notifee;
+        let cache = iface.cache;
+
+        let iface2 = Interface::start().await;
+        // create address book with both of their entries in it
+    }
 
     #[tokio::test]
     async fn it_works() {
@@ -96,14 +175,21 @@ mod tests {
         let client = HttpClientBuilder::default().build(&server_url).unwrap();
 
         println!("{}", client.ping().await.unwrap());
-        client.dm("yo dodo a").await.unwrap();
-        client.dm("YOOOOO").await.unwrap();
-        client.dm("ARE YOU EVEN LISTENING").await.unwrap();
+        client
+            .dm(DirectMessage::new(UserId::new(), "yo dodo a"))
+            .await
+            .unwrap();
         tokio::task::spawn(async move {
             let mut count = 0;
             loop {
                 tokio::time::sleep(Duration::from_secs(3)).await;
-                client.dm(format!("my guy {count}").as_str()).await.unwrap();
+                client
+                    .dm(DirectMessage::new(
+                        UserId::new(),
+                        format!("my guy {count}").as_str(),
+                    ))
+                    .await
+                    .unwrap();
                 count += 1;
             }
         });
--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,1 +1,17 @@
-pub type UserId = u64;
+use std::ops::Deref;
+
+use rand::Rng;
+type PubId = u64;
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, serde::Serialize, serde::Deserialize)]
+pub struct UserId(PubId);
+impl UserId {
+    pub fn new() -> Self {
+        Self(rand::rng().random())
+    }
+}
+impl Deref for UserId {
+    type Target = PubId;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
