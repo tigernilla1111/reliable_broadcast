e6f8d4889d7867983d58acb5772126572b55c8ea
Gilad <gilad@sundaygroupinc.com>
2025-12-18 13:02:01 -0800

    generic data tests

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,8 +1,11 @@
 use jsonrpsee::core::{RpcResult, async_trait};
 use jsonrpsee::proc_macros::rpc;
 use std::collections::HashMap;
+use std::fmt::format;
+use std::time::Duration;
 use std::{net::SocketAddr, sync::Arc};
 use tokio;
+use tokio::net::ToSocketAddrs;
 
 use crate::types::{LedgerDiff, Signature, UserId};
 // use jsonrpsee::core::middleware::RequestBodyLimitLayer;
@@ -47,6 +50,10 @@ mod api {
     }
 }
 
+/// Allows to have an arbitrary amount of Types of messages.
+/// Can reserve a MsgLinkId eg 0 and have gossip comms come over that
+/// TODO maybe: add a reserve system to designate certain LinkIds to an enum the user can define.
+///.      Randomly generated LinkIds cannot be one of the reserves
 type InnerRegistry<T> = HashMap<MsgLinkId, (Sender<MsgLink<T>>, Option<Receiver<MsgLink<T>>>)>;
 #[derive(Clone)]
 pub struct Registry<T> {
@@ -79,10 +86,14 @@ impl<T> Registry<T> {
         });
         rx.take()
     }
-    pub async fn deliver(&self, msg: MsgLink<T>) {
+    pub async fn deliver(&self, msg: MsgLink<T>) -> Result<(), String> {
         if let Some((tx, _)) = self.msg_channel_map.lock().await.get(msg.get_msg_id()) {
-            tx.send(msg).await.unwrap();
+            return tx
+                .send_timeout(msg, Duration::from_millis(100))
+                .await
+                .map_err(|e| e.to_string());
         }
+        return Err(format!("no active channel for {:?}", msg.get_msg_id()));
     }
     pub async fn remove(&self, msg_id: MsgLinkId) {
         self.msg_channel_map.lock().await.remove(&msg_id);
@@ -92,7 +103,17 @@ impl<T> Registry<T> {
 use api::{MsgLinkServer, MyRpcClient, MyRpcServer};
 
 pub trait Data:
-    Send + Sync + serde::Serialize + serde::de::DeserializeOwned + Clone + 'static
+    Send + Sync + serde::Serialize + serde::de::DeserializeOwned + Clone + std::fmt::Debug + 'static
+{
+}
+impl<T> Data for T where
+    T: Send
+        + Sync
+        + serde::Serialize
+        + serde::de::DeserializeOwned
+        + Clone
+        + std::fmt::Debug
+        + 'static
 {
 }
 
@@ -100,16 +121,15 @@ pub trait Data:
 pub enum MsgLinkData {
     Send(Signature, Vec<LedgerDiff>),
 }
-impl Data for MsgLinkData {}
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub struct MsgLink<T> {
-    sender: UserId,
+    sender: SocketAddr,
     msg_id: MsgLinkId,
     data: T,
 }
 impl<T> MsgLink<T> {
-    pub fn new(sender: UserId, req_id: MsgLinkId, data: T) -> Self {
+    pub fn new(sender: SocketAddr, req_id: MsgLinkId, data: T) -> Self {
         Self {
             sender,
             msg_id: req_id,
@@ -135,30 +155,24 @@ pub fn create_msg_link_rpc_module<T: Data>(registry: Registry<T>) -> jsonrpsee::
 }
 pub struct Interface<T> {
     /// My IpAddr
-    _addr: SocketAddr,
-    /// TODO: My PubKey
-    pubkey: UserId,
-    /// Maps a UserId to their addr
-    addr_book: Arc<Mutex<HashMap<UserId, SocketAddr>>>,
+    pub addr: SocketAddr,
     /// Stores clients so I don't have to recreate them for each outgoing connection
     clients: Arc<Mutex<HashMap<SocketAddr, Arc<HttpClient>>>>,
-    registry: Registry<T>,
+    pub registry: Registry<T>,
 }
 impl<T: Data> Interface<T> {
-    pub async fn new() -> Arc<Self> {
+    pub async fn new(addr: impl ToSocketAddrs) -> Arc<Self> {
         let server = ServerBuilder::default()
             .set_http_middleware(
                 tower::ServiceBuilder::new().layer(ConcurrencyLimitLayer::new(1000)), // .layer(RequestBodyLimitLayer::new(1024 * 1024)), // 1 MB
             )
-            .build("127.0.0.1:0")
+            .build(addr)
             .await
             .unwrap();
         let addr = server.local_addr().unwrap();
         let registry = Registry::new();
         let iface = Arc::new(Interface {
-            _addr: addr,
-            addr_book: Arc::new(Mutex::new(HashMap::new())),
-            pubkey: UserId::new(),
+            addr,
             clients: Arc::new(Mutex::new(HashMap::new())),
             registry: registry.clone(),
         });
@@ -171,29 +185,22 @@ impl<T: Data> Interface<T> {
     }
 
     /// Test to have a communication exchange
-    pub async fn send_msg(&self, rcvr: &UserId, msg_data: MsgLinkData, msg_link_id: MsgLinkId) {
-        println!(
-            "{:?}, {}, sending msg to {:?}, {:?}",
-            self.pubkey, self._addr, rcvr, msg_data
-        );
+    pub async fn send_msg(&self, rcvr: &SocketAddr, msg_data: T, msg_link_id: MsgLinkId) {
+        println!("{}, sending msg to {:?}, {:?}", self.addr, rcvr, msg_data);
         let client = self.connect(rcvr).await;
-        let msg_link = MsgLink::new(self.pubkey, msg_link_id, msg_data);
+        let msg_link = MsgLink::new(self.addr, msg_link_id, msg_data);
         self.registry.register(msg_link_id).await;
         client.msg(msg_link).await.unwrap();
     }
 
-    pub async fn connect(&self, rcvr: &UserId) -> Arc<HttpClient> {
-        let addr_book = self.addr_book.lock().await;
-        let addr = addr_book.get(rcvr).unwrap();
+    // TODO: make connect take a socketaddr instead of a pubkey/userId
+    pub async fn connect(&self, addr: &SocketAddr) -> Arc<HttpClient> {
         let mut clients = self.clients.lock().await;
         let client = clients.entry(*addr).or_insert_with(|| {
             let server_url = format!("http://{}", addr);
             Arc::new(HttpClientBuilder::default().build(&server_url).unwrap())
         });
         client.clone()
-    }
-    pub async fn add_addr_book(&self, id: UserId, addr: SocketAddr) {
-        self.addr_book.lock().await.insert(id, addr);
     }
 }
 
@@ -205,31 +212,27 @@ mod tests {
 
     #[tokio::test]
     async fn interface_test() {
-        let iface = Interface::new().await;
-        let addr = iface._addr;
-        let id = iface.pubkey;
+        let iface1 = Interface::new("127.0.0.1:0").await;
+        let addr1 = iface1.addr;
         let msg_id = MsgLinkId::new(68);
 
-        let mut rx = iface.registry.subscribe(msg_id.clone()).await.unwrap();
+        let mut rx = iface1.registry.subscribe(msg_id.clone()).await.unwrap();
 
         tokio::task::spawn(async move {
             loop {
-                let value: MsgLink<MsgLinkData> = rx.recv().await.unwrap();
+                let value: MsgLink<String> = rx.recv().await.unwrap();
                 println!("receved {:?}", value);
             }
         });
 
-        let iface2: Arc<Interface<MsgLinkData>> = Interface::new().await;
-        iface2.add_addr_book(id, addr).await;
-        let msg_link_data = MsgLinkData::Send("sign0x0dj03dm0".to_string(), Vec::new());
-        iface2
-            .send_msg(&id, msg_link_data.clone(), msg_id.clone())
-            .await;
+        let iface2: Arc<Interface<String>> = Interface::new("127.0.0.1:0").await;
+        let msg_data = "test_data_1".to_string();
+        iface2.send_msg(&addr1, msg_data, msg_id.clone()).await;
     }
 
     #[tokio::test]
     async fn registry_subscribe_only_once() {
-        let registry: Registry<MsgLinkData> = Registry::new();
+        let registry: Registry<String> = Registry::new();
         let msg_id = MsgLinkId::new(98);
 
         // First subscribe should succeed
@@ -240,19 +243,20 @@ mod tests {
         let rx2 = registry.subscribe(msg_id).await;
         assert!(rx2.is_none(), "second subscribe should return None");
     }
+
     #[tokio::test]
     async fn registry_send_before_subscribe() {
         let registry = Registry::new();
         let msg_id = MsgLinkId::new(789);
-        let sender = UserId::new();
+        let sender = "192.0.2.1:0".parse().unwrap();
 
-        let msg = MsgLink::new(sender, msg_id, MsgLinkData::Send("sig".to_string(), vec![]));
+        let msg = MsgLink::new(sender, msg_id, "test_message".to_string());
 
         // Register the msg_id but DO NOT subscribe yet
         registry.register(msg_id).await;
 
         // Deliver message before subscription
-        registry.deliver(msg).await;
+        assert!(registry.deliver(msg).await.is_ok());
 
         // Now subscribe
         let mut rx = registry
@@ -268,19 +272,15 @@ mod tests {
 
     #[tokio::test]
     async fn interface_send_before_receiver_subscribes() {
-        let iface1: Arc<Interface<MsgLinkData>> = Interface::new().await;
-        let iface2 = Interface::new().await;
+        let iface1: Arc<Interface<String>> = Interface::new("127.0.0.1:0").await;
+        let iface2 = Interface::new("127.0.0.1:0").await;
 
         // Exchange address
-        iface1.add_addr_book(iface2.pubkey, iface2._addr).await;
-
         let msg_id = MsgLinkId::new(987);
-        let msg_data = MsgLinkData::Send("sig-buffered".to_string(), Vec::new());
+        let msg_data = "buffered_test_data".to_string();
 
         // Send BEFORE iface2 subscribes or registers
-        iface1
-            .send_msg(&iface2.pubkey, msg_data.clone(), msg_id)
-            .await;
+        iface1.send_msg(&iface2.addr, msg_data, msg_id).await;
 
         // Now iface2 subscribes AFTER the message arrived
         let mut rx = iface2
@@ -290,7 +290,7 @@ mod tests {
             .expect("receiver should exist");
 
         // The message should already be buffered
-        let received: MsgLink<MsgLinkData> =
+        let received: MsgLink<String> =
             tokio::time::timeout(std::time::Duration::from_secs(1), rx.recv())
                 .await
                 .expect("timed out waiting for buffered msg")
@@ -298,10 +298,11 @@ mod tests {
 
         assert_eq!(*received.get_msg_id(), msg_id);
     }
+
     #[tokio::test]
     async fn registry_multiple_msglinkids_interleaved_no_signature_usage() {
         let registry = Registry::new();
-        let sender = UserId::new();
+        let sender = "192.0.2.1:0".parse().unwrap();
 
         let msg_id_a = MsgLinkId::new(222);
         let msg_id_b = MsgLinkId::new(333);
@@ -311,28 +312,19 @@ mod tests {
 
         // Interleave deliveries (payload contents don't matter)
         registry
-            .deliver(MsgLink::new(
-                sender,
-                msg_id_a,
-                MsgLinkData::Send("unused".to_string(), vec![]),
-            ))
-            .await;
+            .deliver(MsgLink::new(sender, msg_id_a, "test_a_1".to_string()))
+            .await
+            .unwrap();
 
         registry
-            .deliver(MsgLink::new(
-                sender,
-                msg_id_b,
-                MsgLinkData::Send("unused".to_string(), vec![]),
-            ))
-            .await;
+            .deliver(MsgLink::new(sender, msg_id_b, "test_b_1".to_string()))
+            .await
+            .unwrap();
 
         registry
-            .deliver(MsgLink::new(
-                sender,
-                msg_id_a,
-                MsgLinkData::Send("unused".to_string(), vec![]),
-            ))
-            .await;
+            .deliver(MsgLink::new(sender, msg_id_a, "test_a_2".to_string()))
+            .await
+            .unwrap();
 
         // Subscribe after all messages arrived
         let mut rx_a = registry
@@ -363,5 +355,75 @@ mod tests {
 
         assert_eq!(count_a, 2, "MsgLinkId A should receive exactly 2 messages");
         assert_eq!(count_b, 1, "MsgLinkId B should receive exactly 1 messages");
+    }
+
+    #[tokio::test]
+    async fn interface_multiple_clients_same_msgid() {
+        let iface1: Arc<Interface<String>> = Interface::new("127.0.0.1:0").await;
+        let iface2: Arc<Interface<String>> = Interface::new("127.0.0.1:0").await;
+        let iface3: Arc<Interface<String>> = Interface::new("127.0.0.1:0").await;
+
+        let msg_id = MsgLinkId::new(555);
+        let mut rx = iface1.registry.subscribe(msg_id).await.unwrap();
+
+        // Two different clients send to the same msg_id
+        iface2
+            .send_msg(&iface1.addr, "test_from_client2".to_string(), msg_id)
+            .await;
+
+        iface3
+            .send_msg(&iface1.addr, "test_from_client3".to_string(), msg_id)
+            .await;
+
+        // Should receive both messages
+        let msg1 = tokio::time::timeout(Duration::from_secs(1), rx.recv())
+            .await
+            .expect("timeout")
+            .expect("channel closed");
+
+        let msg2 = tokio::time::timeout(Duration::from_secs(1), rx.recv())
+            .await
+            .expect("timeout")
+            .expect("channel closed");
+
+        // Verify both messages arrived (order may vary)
+        let addrs: Vec<_> = vec![msg1.sender, msg2.sender];
+        assert!(addrs.contains(&iface2.addr));
+        assert!(addrs.contains(&iface3.addr));
+    }
+    #[tokio::test]
+    async fn registry_channel_overflow_behavior() {
+        let registry = Registry::new();
+        let msg_id = MsgLinkId::new(444);
+        let sender = "192.0.2.1:0".parse().unwrap();
+
+        registry.register(msg_id).await;
+
+        // Fill the channel beyond MAX_MSGS_PER_LINK_ID
+        for i in 0..(MAX_MSGS_PER_LINK_ID + 5) {
+            let _ = registry
+                .deliver(MsgLink::new(
+                    sender,
+                    msg_id,
+                    MsgLinkData::Send(format!("msg-{}", i), vec![]),
+                ))
+                .await;
+        }
+
+        let mut rx = registry
+            .subscribe(msg_id)
+            .await
+            .expect("receiver should exist");
+
+        // Count how many messages were actually buffered
+        let mut count = 0;
+        while let Ok(Some(_)) =
+            tokio::time::timeout(std::time::Duration::from_millis(50), rx.recv()).await
+        {
+            count += 1;
+        }
+
+        // Should have received MAX_MSGS_PER_LINK_ID messages
+        assert_eq!(count, MAX_MSGS_PER_LINK_ID);
     }
 }
